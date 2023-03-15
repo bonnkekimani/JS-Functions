ff7f919514d6468594c3f732715659335666349e
Jashin254 <126466991+Jashin254@users.noreply.github.com>
2023-03-15 11:05:54 +0300

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,9 @@
+function sumArray(array){
+    let sum = 0 
+    for (let i =0; i<array.length;i++){
+        sum += ourArray[i];
+    }
+    return sum;
+}
+ let ourArray =[1,9,2,5,4];
+console.log(sumArray(ourArray));
